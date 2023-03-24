ed0ff1e2d2783826d9f47306d4f1d51a723fbf8d
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-03-24 23:49:23 +0300

    timer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 
 function App() {
-  let countDownDate = new Date("Apr 20, 2023 23:59:59").getTime();
+  // let countDownDate = new Date("Apr 20, 2023 23:59:59").getTime();
 
-  let counter = setInterval(() => {
-    let dateNow = new Date().getTime();
+  // let counter = setInterval(() => {
+  //   let dateNow = new Date().getTime();
 
-    let dateDiff = countDownDate - dateNow;
+  //   let dateDiff = countDownDate - dateNow;
 
-    let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
+  //   let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
 
-    document.querySelector(".days").innerHTML = days;
-  })
+  //   document.querySelector(".days").innerHTML = days;
+  // })
 
   return (
     <div className="App">
