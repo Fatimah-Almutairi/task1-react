0f1dcb3d401e166c1737aca716b8a160abf6c6c9
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-03-24 23:47:17 +0300

    react

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 function App() {
   let countDownDate = new Date("Apr 20, 2023 23:59:59").getTime();
 
+  let counter = setInterval(() => {
+    let dateNow = new Date().getTime();
+
+    let dateDiff = countDownDate - dateNow;
+  })
+
   return (
     <div className="App">
 
