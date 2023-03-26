4ab1acebbcf72beca3eae903c6c7db818ceb6d95
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-03-26 23:42:00 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import './App.css';
+// import './App.css';
 
-function App() {
-  // let countDownDate = new Date("Apr 20, 2023 23:59:59").getTime();
+// function App() {
+//   // let countDownDate = new Date("Apr 20, 2023 23:59:59").getTime();
 
-  // let counter = setInterval(() => {
-  //   let dateNow = new Date().getTime();
+//   // let counter = setInterval(() => {
+//   //   let dateNow = new Date().getTime();
 
-  //   let dateDiff = countDownDate - dateNow;
+//   //   let dateDiff = countDownDate - dateNow;
 
-  //   let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+//   //   let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
 
-  //   document.querySelector(".days").innerHTML = days;
-  // }, 1000)
+//   //   document.querySelector(".days").innerHTML = days;
+//   // }, 1000)
 
-  return (
-    <div className="App">
+//   return (
+//     <div className="App">
 
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
