81b854e5013d8fe8a9df80ec90f97765a342c9b8
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-03-29 01:33:37 +0300

    count

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,16 @@ import './App.css'
 
 function App() {
  
+  let countDownDate = new Date (' Apr 20 , 2023 , 23:59:59 ').getTime();
+  let counter = setInterval( () => {
+    let dateNow = new Date().getTime();
+
+    let dateDiff = countDownDate - dateNow;
+
+    let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor ((dateDiff % ( 1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes
+  }, 1000)
   return (
     <div className="App">
       
